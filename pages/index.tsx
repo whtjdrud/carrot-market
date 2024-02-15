2d30e0feac7fdd0303f1df0ce3326f11eb53edd1
whtjdrud <151587265+whtjdrud@users.noreply.github.com>
2024-02-16 01:46:03 +0900

    :tada: begin a project!

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black">it works</h1>
+    </div>
+  )
+}
+
+export default Home
